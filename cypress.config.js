4d16d31468bd59691c10204e71ff8e39344f12c3
Antonio Gómez Gallardo <antoniogomezgallardo@MacBook-Pro-de-Antonio.local>
2024-05-05 07:38:20 +0200

    Added changes in config file to add videos

--- cypress.config.js
+++ cypress.config.js
@@ -7,6 +7,7 @@ module.exports = defineConfig({
     url: 'https://rahulshettyacademy.com'
   },
   reporter: 'cypress-mochawesome-reporter',
+  video: true,
 
   e2e: {
     setupNodeEvents(on, config) {
